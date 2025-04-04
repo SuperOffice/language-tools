2fe7b6fe3fee94346e00a8da75f225c253d689ca
Eivind Fasting <eivind.fasting@superoffice.com>
2025-04-04 10:42:06 +0200

    Added signaturehelp for membercalls

--- packages/langium-crmscript/src/language/lsp/signaturehelp-provider.ts
+++ packages/langium-crmscript/src/language/lsp/signaturehelp-provider.ts
@@ -1,7 +1,7 @@
 import { AstNode, CstNode, CstUtils, MaybePromise } from 'langium';
 import { AbstractSignatureHelpProvider, } from 'langium/lsp';
 import { ParameterInformation, SignatureHelp, SignatureInformation} from 'vscode-languageserver';
-import { ConstructorCall, isConstructorCall } from '../generated/ast.js';
+import { ConstructorCall, isConstructorCall, isMemberCall, MemberCall, MethodMember } from '../generated/ast.js';
 
 
 export const DefaultNameRegexp = /^[\w\p{L}]$/u;
@@ -12,27 +12,16 @@ export class CrmscriptSignatureHelpProvider extends AbstractSignatureHelpProvide
         if (isConstructorCall(element)) {
             return this.generateSignatureHelpFromConstructors(element);
         }        
-
-        // const signatureHelp: SignatureHelp = { signatures: [], activeSignature: undefined, activeParameter: undefined };
-
-        // const constructorCall = element as ConstructorCall;
-        // constructorCall.type.ref?.constructors.forEach(constructorElements => {
-        //         const label = `${constructorElements.$container.name}(` +
-        //         constructorElements.parameters.map(p => `${p.type.$refText} ${p.name}`).join(', ') +
-        //     `)`;
-
-        //     const documentation = CstUtils.findCommentNode(constructorElements.$cstNode, ['ML_COMMENT']);
-            
-        //     const option = SignatureInformation.create(label, documentation?.text, ParameterInformation.create([constructorCall.type.$refText.length + 1, label.length - 1]));
-        //     signatureHelp.signatures.push(option);
-        // });
-        // return signatureHelp;
+        if(isMemberCall(element)){
+            return this.generateSignatureHelpFromMemberCall(element);
+        }
         return undefined;
     }
 
     generateSignatureHelpFromConstructors(element: AstNode): MaybePromise<SignatureHelp | undefined> {
         const signatureHelp: SignatureHelp = { signatures: [], activeSignature: undefined, activeParameter: undefined };
         const constructorCall = element as ConstructorCall;
+        
         constructorCall.type.ref?.constructors.forEach(constructorElements => {
                 const label = `${constructorElements.$container.name}(` +
                 constructorElements.parameters.map(p => `${p.type.$refText} ${p.name}`).join(', ') +
@@ -43,6 +32,27 @@ export class CrmscriptSignatureHelpProvider extends AbstractSignatureHelpProvide
             const option = SignatureInformation.create(label, documentationText, ParameterInformation.create([constructorCall.type.$refText.length + 1, label.length - 1]));
             signatureHelp.signatures.push(option);
         });
+        return signatureHelp;
+    }
+
+    generateSignatureHelpFromMemberCall(element: AstNode): MaybePromise<SignatureHelp | undefined> {
+        const signatureHelp: SignatureHelp = { signatures: [], activeSignature: undefined, activeParameter: undefined };
+        const memberCall = element as MemberCall;
+        const documentationText = this.getDocumentationTextFromNode(memberCall.element?.ref?.$cstNode);
+        const methodMember = memberCall.element?.ref as MethodMember;
+
+        const parametersString = methodMember.parameters.map(p => `${p.type.$refText} ${p.name}`).join(', ');
+        const label = `${methodMember.name}(` + parametersString + `)`; 
+
+        let start = methodMember.name.length + 1;
+        let end = start + parametersString.length;
+        if(parametersString.length === 0){
+            start = 0;
+            end = 0;
+        }
+
+        const option = SignatureInformation.create(label, documentationText, ParameterInformation.create([start, end]));
+        signatureHelp.signatures.push(option);
         return signatureHelp;
     }
 
