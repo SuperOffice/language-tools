789d9b53b3bd73be83039a8fcff6fb849b542e23
Eivind Fasting <eivind.fasting@superoffice.com>
2025-10-09 21:53:05 +0200

    Added vitest to create unit tests that do not have a dependency on @vscode/test-cli

---
+++ packages/language-server/vitest.config.ts
@@ -0,0 +1,9 @@
+// packages/utils/vitest.config.ts
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+    test: {
+        include: ['test/**/*.test.ts'],
+        environment: 'node'
+    }
+});
